b9bd0fb06536330cacccba207b9ed1916b0c1c77
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2023-06-18 18:56:02 +0530

    linkedinOauth

--- src/pages/Badges.tsx
+++ src/pages/Badges.tsx
@@ -10,6 +10,12 @@ const Badges: FunctionComponent = () => {
   const github=()=>{
     window.open("http://localhost:5000/auth/github","newwindow","width=600,height=600")
   }
+  const twitter=()=>{
+    window.open("http://localhost:5000/auth/twitter","newwindow","width=600,height=600")
+  }
+  const linkedin=()=>{
+    window.open("http://localhost:5000/auth/linkedin","newwindow","width=600,height=600")
+  }
   return (
     <div className={styles.badges}>
       <div className={styles.badgesChild} />
@@ -41,15 +47,15 @@ const Badges: FunctionComponent = () => {
           </div>
         </div>
       </div>
-      {/* <div className={styles.firstCard}>
-        <Zixins name="twitter" description="Twitter Auth Badge" imgsrc="/background.svg"/>
+      <div className={styles.firstCard}>
+        <Zixins name="twitter" description="Twitter Auth Badge" imgsrc="/background.svg" click={twitter}/>
       </div>
-      <div className={styles.firstCard1}>
+      {/* <div className={styles.firstCard1}>
         <Zixins name="Instagram" description="Instagram Auth Badge" imgsrc="/background1.svg"/>
-      </div>
-      <div className={styles.firstCard2}>
-        <Zixins name="linkedin" description="Linkedin Auth Badge" imgsrc="/background2.svg"/>
       </div> */}
+      <div className={styles.firstCard2}>
+        <Zixins name="linkedin" description="Linkedin Auth Badge" imgsrc="/background2.svg" click={linkedin}/>
+      </div>
       <div className={styles.firstCard3}>
         <Zixins name="Google" description="Google Auth Badge" imgsrc="/background3@2x.png" click={google}/>
       </div>
